74704fd6f2bd60946abaa045775fa95ef67da93d
Daniel Abramov <daniel.abramov@element.io>
2023-09-03 18:37:46 +0200

    sdk(widget): always reply to widget messages
    Regardless of the incoming message being correct or not. This
    addresses the points from @toger5 partially addressed in his PR:
    https://github.com/matrix-org/matrix-rust-sdk/pull/2493/files

--- crates/matrix-sdk/src/widget/client/handler/mod.rs
+++ crates/matrix-sdk/src/widget/client/handler/mod.rs
@@ -11,7 +11,10 @@ use self::state::State;
 pub(crate) use self::{
     capabilities::Capabilities,
     error::{Error, Result},
-    incoming::{Request as IncomingRequest, Response as IncomingResponse},
+    incoming::{
+        ErrorResponse as IncomingErrorResponse, Request as IncomingRequest,
+        Response as IncomingResponse,
+    },
     openid::{OpenIdDecision, OpenIdStatus},
     outgoing::Request as OutgoingRequest,
 };
@@ -61,29 +64,27 @@ impl MessageHandler {
     }
 
     /// Handles incoming messages from a widget.
-    pub(crate) async fn handle(&self, header: Header, action: Action) -> Result<()> {
-        // First let's try to convert the incoming message from a widget into a proper
-        // "validated" message, i.e. `IncomingRequest`. If this conversion fails, then
-        // it means that the widget sent the message that the widget is not supposed to
-        // send. We ensure that we only process correct incoming requests.
-        match IncomingRequest::new(header, action).ok_or(Error::custom("Invalid message"))? {
+    pub(crate) async fn handle(&self, header: Header, action: Action) {
+        // Validate the message. Note, that we ignore the error, because the only error
+        // that can be returned here is `Err(())`, which means that the widget is
+        // disconnected, which does not need to be handled in any way at the moment.
+        let _ = match IncomingRequest::new(header, action) {
             // Normally, we send all incoming requests to the worker task (`State::listen`), but the
-            // `SupportedApiVersions` request is a special case - not only the widget
-            // can request them at any time, but it also may block the processing of
-            // other messages until we reply with the supported API versions.
-            // Luckily, this request is the only single request that does not require
-            // any state (as its answer is essentially 'static'), so we can handle this message
-            // right away by generating and sending an appropriate response.
-            IncomingRequest::GetSupportedApiVersion(req) => self
-                .widget
-                .reply(req.map(Ok(SupportedApiVersions::new())))
-                .await
-                .map_err(|_| Error::WidgetDisconnected),
+            // `SupportedApiVersions` request is a special case - not only the widget can send it
+            // at any time, but it also may block the processing of other messages until we reply
+            // to this one. Luckily, this request is the only single one that does not depend on
+            // any state, so we can handle the message right away.
+            Ok(IncomingRequest::GetSupportedApiVersion(req)) => {
+                self.widget.reply(req.map(Ok(SupportedApiVersions::new()))).await.map_err(|_| ())
+            }
             // Otherwise, send the incoming request to a worker task. This way our
             // `self.handle()` should actually never block. So the caller can call it many times in
             // a row and it's the `State` (that runs in its own task) that will decide which of
             // them to process sequentially and which in parallel.
-            request => self.state_tx.send(request).map_err(|_| Error::WidgetDisconnected),
-        }
+            Ok(request) => self.state_tx.send(request).map_err(|_| ()),
+            // An error here means that the `header` + `action` pair did not constitute a valid
+            // incoming request, so we just report this back to the widget as an error.
+            Err(err) => self.widget.reply(err).await.map_err(|_| ()),
+        };
     }
 }
